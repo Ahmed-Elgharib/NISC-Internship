b5f782d0c0bc489858c0be0b1a27136eb5ad57cb
AmrElsherbiny <64423818+AmrElsherbiny@users.noreply.github.com>
2022-09-04 16:27:41 +0200

    Add files via upload

--- Gateway System/BluetoothProgram.py
+++ Gateway System/BluetoothProgram.py
@@ -1,91 +1,72 @@
-from bluetooth.ble import GATTRequester
 from bluepy.btle import Peripheral
 from bluepy.btle import Scanner
 from datetime import datetime
 from threading import Thread
-from subprocess import run
+from subprocess import Popen
 from time import sleep
 from json import loads
 import requests
 
-scanner = Scanner()
-refreshing = False
+scanner = None
+post_id = {}
 
 def RefreshBluetooth():
-    global refreshing
-    refreshing = True
-    run("hciconfig hci0 down")
-    run("hciconfig hci0 up")
-    run("service bluetooth restart")
-    refreshing = False
+    Popen(["hciconfig","hci0","down"])
+    Popen(["hciconfig","hci0","up"])
+    Popen(["service","bluetooth","restart"])
 
-refbt = Thread(target=RefreshBluetooth)
+def BTScanner():
+    global scanner,scanning
+    while(True):
+        try:
+            scanner = Scanner()
+            scanner.scan(0,True)
+        except:
+            print("Error: Failed to initiate bluetooth scanner, retrying again...")
+            RefreshBluetooth()
 
-def MainProgram():
-    global refreshing
-    global scanner
+def JSONRequest(mac,info={}):
+    global post_id
+    url = "http://computer:3000/readings/"
+    try:
+        if mac not in post_id:
+            req = requests.post(url,json={"mac":mac})
+            if not req.ok: return
+            post_id[mac] = loads(req.text)['id']
+        else:
+            req = requests.patch("".join((url,str(post_id[mac]))),data=info)
+            if not req.ok:
+                req = requests.post(url,json={"mac":mac})
+                if not req.ok: return
+                post_id[mac] = loads(req.text)['id']
+    except: return
+def EstablishedBTDevice(btd,mac):
+    Thread(target=JSONRequest,args=(mac,)).start()
+    val = b'\x01'
+    info = {}
+    while(btd.getState() == "conn"):
+        btd.writeCharacteristic(0x0020,val)
+        btd.writeCharacteristic(0x0022,val)
+        info['led0'] = btd.readCharacteristic(0x0020)
+        info['led1'] = btd.readCharacteristic(0x0022)
+        info['last-update'] = datetime.now()
+        Thread(target=JSONRequest,args=(mac,info)).start()
+        if val == b'\x01': val = b'\x00'
+        else: val = b'\x01'
+        sleep(0.5)
                 
-    scnbtd.start()
-
+def MainProgram():
+    global scanner,scanning
+    Thread(target=BTScanner).start()
+    while(scanner is None): pass
     mac_list = ["b0:91:22:69:ff:fe","54:6c:0e:9b:69:be"]
-
     while(True):
         if len(scanner.scanned) < 1: continue
-        nextscan = next(iter(scanner.scanned))
-        device = scanner.scanned[nextscan]
-        print(f"MAC: {device.addr} ({device.addrType}) with RSSI {device.rssi} dB")
-        del scanner.scanned[nextscan]
-        if (nextscan not in mac_list): continue
-        #device = scanner.scanned.pop(nextscan)
-        try:
-            req = GATTRequester(nextscan,False)
-            req.connect(True)
-            Thread(target=EstablishedBTDevice,args=(req,nextscan)).start()
-        except: pass
-        
-main = Thread(target=MainProgram)
-
-def ScanBTDevices():
-    global refreshing
-    global scanner
-    while(True):
-        try:
-            scanner.start(passive=True)
-            scanner.process(0)
-        except:
-            try:
-                scanner.stop()
-                scanner.clear()
-            except:
-                while(True):
-                    try:
-                        scanner = Scanner()
-                        break
-                    except:
-                        if Refreshing == False:
-                            refbt.start()
-        
-scnbtd = Thread(target=ScanBTDevices)
-
-def EstablishedBTDevice(req,mac):
-    try:
-        url = "http://computer:3000/readings/"
-        _id = loads(requests.post(url,json={"mac":mac}).text)['id']
-        url = "".join((url,str(_id)))
-        val = b'\x01'
-        info = {}
-        while(req.is_connected()):
-            req.write_by_handle(0x0020,val)
-            req.write_by_handle(0x0022,val)
-            info['led0'] = req.read_by_handle(0x0020)
-            info['led1'] = req.read_by_handle(0x0022)
-            info['last-update'] = datetime.now()
-            requests.patch(url,data=info)
-            sleep(0.5)
-            if(val==b'\x01'):
-                val = b'\x00'
-            else:
-                val = b'\x01'
-    except: return
+        avail_mac = mac_list & scanner.scanned.keys()
+        if len(avail_mac) < 1: continue
+        for mac in avail_mac:
+            print(f"Available MAC: {mac} ({scanner.scanned[mac].addrType}) with RSSI: {scanner.scanned[mac].rssi} dB")
+            Thread(target=EstablishedBTDevice,args=(Peripheral(mac,"public"),mac)).start()
+            del scanner.scanned[mac]
                 
-main.start()
+Thread(target=MainProgram).start()
