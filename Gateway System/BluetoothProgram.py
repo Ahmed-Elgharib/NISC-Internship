e9da3e587e2c54531cda4612ff9d0df3dcc57e42
AmrElsherbiny <64423818+AmrElsherbiny@users.noreply.github.com>
2022-09-22 14:02:48 +0200

    Add files via upload

--- Gateway System/BluetoothProgram.py
+++ Gateway System/BluetoothProgram.py
@@ -8,107 +8,102 @@ from time import sleep
 from json import loads
 import requests
 
+# Global variables
 scanner = None
 dev_data = {}
 
 
-def RefreshBluetooth():
-    Popen(["hciconfig", "hci0", "down"])
-    Popen(["hciconfig", "hci0", "up"])
-    Popen(["service", "bluetooth", "restart"])
+def RefreshBluetooth(): # Warning: This function is risky as the program may stop, better refresh manually
+    Popen(["hciconfig", "hci0", "down"])  # Shut down the bluetooth driver
+    Popen(["hciconfig", "hci0", "up"])  # Start up the bluetooth driver
+    Popen(["service", "bluetooth", "restart"])  # Restart bluetooth service
 
 
 def BTScanner():
     global scanner
+    # RefreshBluetooth() # Refresh the bluetooth initially (Disabled here)
     while (True):
         try:
-            scanner = Scanner()
-            scanner.scan(0, True)
+            scanner = Scanner()  # Initialize the scanner
+            scanner.scan(0, True) # Start scanning for infinity with passive set to True
         except:
             print("Error: Failed to initiate bluetooth scanner, retrying again...")
-            RefreshBluetooth()
+            RefreshBluetooth()  # Try refreshing the bluetooth itself in case errors or failures
 
 
-def MongoDBClient():
+def MongoDBClient():  # For the Mongo Database Storage Document
     global dev_data
     while (True):
         try:
-            clnt = MongoClient("mongodb://computer/")
-            db = clnt["mydb"]
-            col = db["readings"]
-            print("Connected to Mongo DataBase!")
+            clnt = MongoClient("mongodb://computer/") # Connect to the Mongo Database link
+            print("Connected to Mongo database!")
+        except:
+            print("Error connecting to Mongo database, retrying...")
+            continue
+        try:
+            col = clnt["mydb"]["readings"] # Select from database "mydb" collection named "readings"
             while (True):
                 for mac in dev_data:
                     if not list(col.find({"mac": mac})):
-                        col.insert_one({"mac": mac})
+                        col.insert_one(dev_data[mac]) # Insert data initially when does not exist
                     for var in dev_data[mac]:
-                        col.update_one(
-                            {"mac": mac}, {"$set": {var: dev_data[mac][var]}})
-                        del dev_data[mac][var]
-                    col.update_one(
-                        {"mac": mac}, {"$set": {"last-updated": datetime.now()}})
-        except:
-            print("Error connecting to Mongo DataBase, retrying...")
+                        col.update_one({"mac": mac}, {"$set": {var: dev_data[mac][var]}}) # Update every variable inside data document
+                    col.update_one({"mac": mac}, {"$set": {"last-updated": datetime.now().ctime()}}) # Give out the last date and time the data document was updated
+        except: print("Error updating data to Mongo database, reconnecting...")
 
 
-def JSONDBClient():
+def JSONDBClient():  # For the JSON Database Web Document
     global dev_data
-    url = "http://computer:3000/readings"
+    url = "http://computer:3000/readings"  # Link for JSON Server Database
     while (True):
-        sleep(1)
-        while (not dev_data):
-            pass
+        sleep(1)  # Iterate the loop every 1 second
+        while (not dev_data): pass  # Pause as long as there is no data entered yet from devices
         while (True):
             try:
-                req = requests.get(url)
-                if req.ok:
-                    break
-            except:
-                pass
+                req = requests.get(url)  # Check if is able to get data by request
+                if req.ok: break  # Break the loop once successful
+            except: pass
         for info in loads(req.text):
-            requests.delete("".join((url, "/", str(info['id']))))
+            requests.delete("".join((url, "/", str(info['id'])))) # Delete all the old data document
         for mac in dev_data:
-            req = requests.post(url, data=dev_data[mac])
-            dur = req.elapsed.total_seconds()*1000
-            print("Time taken for uploading data into database:", dur, "ms")
+            req = requests.post(url, data=dev_data[mac]) # Enter the new data into web document
+            dur = req.elapsed.total_seconds()*1000 # Get the total time it took from start of post till response
+            dev_data[mac]["RTT"] = dur # Set the round trip time taken for the specific data post request
+            print("Time taken for uploading data into JSON database:", dur, "ms")
 
 
-def EstablishedBTDevice(btd, mac):
-    global dev_data
-    val = b'\x01'
+def EstablishedBTDevice(btd, mac, addrType, rssi):
+    global dev_data  # Initialization
+    # Below is to create a new dictionary list for the new MAC address device with scanner information
+    dev_data[mac] = {"node-id": "-", "protocol": "ble", "addrType": addrType,
+                     "rssi": rssi, "mac": mac, "sensor-id": "-", "value": "-",
+                     "magnitude": "-", "gate-id": "-", "network-id": "-"}
     print("Bluetooth connection established with device MAC Address:", mac)
-    while (btd.getState() == "conn"):
-        btd.writeCharacteristic(0x0020, val)
-        btd.writeCharacteristic(0x0022, val)
-        if mac not in dev_data:
-            dev_data[mac] = {}
-        dev_data[mac]['led0'] = btd.readCharacteristic(0x0020)
-        dev_data[mac]['led1'] = btd.readCharacteristic(0x0022)
-        if val == b'\x01':
-            val = b'\x00'
-        else:
-            val = b'\x01'
-        sleep(0.5)
-
+    try:
+        while (btd.getState() == "conn"):  # Do while loop until connection is lost
+            reading = btd.readCharacteristic(0x002c) # Read the data value from given characteristic handle
+            # reading_hex = b2a_hex(reading).decode() # Filter the reading to give out hex numbers only
+            # reading_int = int(reading_hex,16) # Convert hex into integer
+            reading_int = int.from_bytes(bytearray(reading), byteorder="little") # Enhanced version of converting hex into integer
+            num_bytes = (reading_int.bit_length()+7)//8 # Get total number of bytes
+            reading_str = reading_int.to_bytes(num_bytes, "little").decode()  # Get it in string format
+            dev_data[mac]['sensor-id'] = "Temperature" # Set sensor id name to "Temperature"
+            dev_data[mac]['value'] = reading_str # We need to read it as a string here
+            dev_data[mac]['magnitude'] = "Celcius" # Set magnitude named "Celcius"
+        del dev_data[mac]  # Delete when loop ends
+    except: del dev_data[mac]  # Delete when error happens
 
 Thread(target=BTScanner).start()
 Thread(target=MongoDBClient).start()
 Thread(target=JSONDBClient).start()
-while (scanner is None):
-    pass
+while (scanner is None): pass
 mac_list = ["b0:91:22:69:ff:fe", "54:6c:0e:9b:69:be"]
 while (True):
-    avail_mac = mac_list & scanner.scanned.keys()
-    for mac in avail_mac:
-        if mac not in dev_data:
-            dev_data[mac] = {}
-        dev_data[mac]['addrType'] = scanner.scanned[mac].addrType
-        dev_data[mac]['rssi'] = scanner.scanned[mac].rssi
-        print("Found device with MAC Address:", mac,
-              "and RSSI:", scanner.scanned[mac].rssi, "dBs")
-        # The scanner keeps reading the device information until the connection is established (As tested)
-        try:
-            Thread(target=EstablishedBTDevice, args=(
-                Peripheral(mac), mac)).start()
-        except:
-            del scanner.scanned[mac]
+    avail_mac = mac_list & scanner.scanned.keys()  # Get the matched MAC addresses
+    for mac in avail_mac:  # Go iterate through all the founded MAC addresses
+        # These data will be displayed and kept changing until the connection is established
+        addrType = scanner.scanned[mac].addrType # Store address type in temporary variable
+        rssi = scanner.scanned[mac].rssi # Store RSSI in temporary variable as well
+        print("Found device with MAC Address:", mac,"Type:", addrType, "and RSSI:", rssi, "dBs")
+        try: Thread(target=EstablishedBTDevice, args=(Peripheral(mac), mac, addrType, rssi)).start()  # Start the connection thread
+        except: del scanner.scanned[mac] # Remove the MAC address from scanner until shown again
