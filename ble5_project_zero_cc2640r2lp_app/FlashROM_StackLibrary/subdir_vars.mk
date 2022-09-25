b5eb8444bd5bd1c7b48d13fc70bec35dea829b2a
Ahmed-Elgharib <ahm.elgharib@gmail.com>
2022-09-25 14:22:27 +0200

    Final Smart metering project code

---
+++ ble5_project_zero_cc2640r2lp_app/FlashROM_StackLibrary/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../sunlightService.c 
+
+C_DEPS += \
+./sunlightService.d 
+
+OBJS += \
+./sunlightService.obj 
+
+OBJS__QUOTED += \
+"sunlightService.obj" 
+
+C_DEPS__QUOTED += \
+"sunlightService.d" 
+
+C_SRCS__QUOTED += \
+"../sunlightService.c" 
+
+
---
+++ ble5_project_zero_cc2640r2lp_app/FlashROM_StackLibrary/sunlightService.d
@@ -0,0 +1,258 @@
+# FIXED
+
+sunlightService.obj: ../sunlightService.c
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/string.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/xlocale/_string.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/bcomdef.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/comdef.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/cc26xx/_hal_types.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/inc/hal_defs.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/OSAL.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/limits.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/osal_memory.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/osal_timers.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/src/inc/icall.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdlib.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/inc/hal_assert.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/linkdb.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/att.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/l2cap.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gatt.h
+sunlightService.obj: E:/ti/ble5_project_zero_cc2640r2lp_app/PROFILES/gatt_uuid.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gattservapp.h
+sunlightService.obj: E:/ti/ble5_project_zero_cc2640r2lp_app/Include/gapbondmgr.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gap.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/sm.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/hci.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_ae.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/cc26xx/rf_hal.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ble.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_wl.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_common.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/rf/RF.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/dpl/ClockP.h
+sunlightService.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/dpl/SemaphoreP.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/utils/List.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/DeviceFamily.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_common_cmd.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_mailbox.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_prop_cmd.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_ble_cmd.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_scheduler.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_config.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_user_config.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/inc/ble_user_config.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/src/inc/icall_user_config.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/inc/ble_dispatch.h
+sunlightService.obj: E:/ti/ble5_project_zero_cc2640r2lp_app/Startup/board.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/ADC.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/ADCBuf.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/PWM.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/SPI.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/UART.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/Watchdog.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/Board.h
+sunlightService.obj: E:/ti/ble5_project_zero_cc2640r2lp_app/Startup/CC2640R2_LAUNCHXL.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/PIN.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/ioc.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_types.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_chip_def.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_memmap.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_ioc.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_ints.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/interrupt.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_nvic.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/debug.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/cpu.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_cpu_scs.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rom.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/gpio.h
+sunlightService.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_gpio.h
+sunlightService.obj: ../sunlightService.h
+
+../sunlightService.c:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/string.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/xlocale/_string.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/bcomdef.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/comdef.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/cc26xx/_hal_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/inc/hal_defs.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/OSAL.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/limits.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/osal_memory.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/osal/src/inc/osal_timers.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/src/inc/icall.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdlib.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/inc/hal_assert.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/linkdb.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/att.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/l2cap.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gatt.h:
+
+E:/ti/ble5_project_zero_cc2640r2lp_app/PROFILES/gatt_uuid.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gattservapp.h:
+
+E:/ti/ble5_project_zero_cc2640r2lp_app/Include/gapbondmgr.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/gap.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/sm.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/inc/hci.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_ae.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/cc26xx/rf_hal.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/hal/src/target/_common/hal_types.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ble.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_wl.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_common.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/rf/RF.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/dpl/ClockP.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/dpl/SemaphoreP.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/utils/List.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/DeviceFamily.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_common_cmd.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_mailbox.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_prop_cmd.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rf_ble_cmd.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_scheduler.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_config.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/controller/cc26xx/inc/ll_user_config.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/inc/ble_user_config.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/src/inc/icall_user_config.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/icall/inc/ble_dispatch.h:
+
+E:/ti/ble5_project_zero_cc2640r2lp_app/Startup/board.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/ADC.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/ADCBuf.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/PWM.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/SPI.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/UART.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/Watchdog.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/Board.h:
+
+E:/ti/ble5_project_zero_cc2640r2lp_app/Startup/CC2640R2_LAUNCHXL.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/drivers/PIN.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/ioc.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_types.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_chip_def.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_memmap.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_ints.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/interrupt.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_nvic.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/debug.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/cpu.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_cpu_scs.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/rom.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/driverlib/gpio.h:
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/devices/cc26x0r2/inc/hw_gpio.h:
+
+../sunlightService.h:
+
